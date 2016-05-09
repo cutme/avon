153c91f71027e7fd6e74e95f4b32706bdb8a5d41
bartosz mediger <admin@MacBook-Pro-bartosz.local>
2016-05-09 17:22:49 +0200

    decrease font, privacy uncheck, detect document left

--- js/main.js
+++ js/main.js
@@ -1,26 +1,61 @@
 /*jshint expr:true */
-
 jQuery(function($) {
-	function exist(o) {
-		d = ($(o).length>0) ? true : false;
-		return d;
+	function setCookie(name, value, expires) {
+		var cookieStr = escape(name) + "=";
+		if (typeof value != "undefined") {
+			cookieStr += escape(value);
+		}
+		if (!expires) {
+			expires = new Date();
+			expires.setTime(expires.getTime() + 24 * 60 * 60 * 1000);
+		}
+		cookieStr += "; expires=" + expires.toGMTString() + ";";
+		document.cookie = cookieStr;
 	}
+
+	function getCookie(name) {
+		var str = '; ' + document.cookie + ';';
+		var index = str.indexOf('; ' + escape(name) + '=');
+		if (index != -1) {
+			index += name.length + 3;
+			var value = str.slice(index, str.indexOf(';', index));
+			return unescape(value);
+		}
+	}
+
 	var L = {
 		modal: function() {
-			$.magnificPopup.open({
-                items: {
-                    src: '.c-popup--myths',
-                    type: 'inline'
-                },
-				mainClass: 'mfp-fade mfp-modal'
-            });
+			document.addEventListener('mouseout', function(e) {
+				var top = e.pageY;
+				// Jesli kursor poza gorna krawedzia okna
+				if (top < 10) {
+					//Sprawdzamy czy istnieje ciastko o zamknieciu popa.
+					var popClosed = getCookie("popClosed");
+					// W przypadku, gdy ciastko nie istnieje.
+					if (popClosed != 1) {
+						// Odpal popup      
+						$.magnificPopup.open({
+							items: {
+								src: '.c-popup--myths',
+								type: 'inline'
+							},
+							callbacks: {
+								close: function() {
+									setCookie("popClosed", 1);
+								}
+								// e.t.c.
+							},
+							mainClass: 'mfp-fade mfp-modal'
+						});
+					}
+				}
+			});
 		},
 		init: function() {
-			exist('.c-popup') && L.modal();
-			
+			L.modal();
 		}
 	};
 	$(document).ready(function() {
-		L.init();		
+		L.init();
 	});
 });
